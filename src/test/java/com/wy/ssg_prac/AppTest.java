9c15f0cc8235f99592c9a39daacfaf17f15bb734
leewooyup <zjadlspun1114@naver.com>
2022-07-08 14:35:04 +0900

    출력을 리ë다이렉션하여 결과를 문자열로 받기

--- src/test/java/com/wy/ssg_prac/AppTest.java
+++ src/test/java/com/wy/ssg_prac/AppTest.java
@@ -2,8 +2,7 @@ package com.wy.ssg_prac;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -32,5 +31,22 @@ public class AppTest {
         assertEquals("등록", cmd);
         assertEquals("명언1", contents);
         assertEquals("작가1", author);
+    }
+
+    @Test
+    public void 표준출력을_리다이렉션하여_결과를_문자열로_받기() throws IOException {
+        // 표준출력을 리다이렉션
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        System.out.println("안녕");
+
+        String rs = output.toString().trim();
+
+        // 표준출력을 원상복구
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+        output.close();
+
+        assertEquals("안녕", rs);
     }
 }
