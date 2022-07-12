ee2c41cdc8a36354bd894c01fe36e0fdbb62ec2a
leewooyup <zjadlspun1114@naver.com>
2022-07-12 11:42:22 +0900

    App 리펙토링

--- src/main/java/com/wy/ssg_prac/App.java
+++ src/main/java/com/wy/ssg_prac/App.java
@@ -5,13 +5,18 @@ import java.util.List;
 import java.util.Scanner;
 
 public class App {
+    private Scanner sc;
+    private List<WiseSaying> wiseSayings;
+    private int wiseSayingLastId;
+    public App() {
+        sc = new Scanner(System.in);
+        wiseSayings = new ArrayList<>();
+        wiseSayingLastId = 0;
+    }
     public void run() {
         System.out.println("== 명언 SSG ==");
 
-        Scanner sc = new Scanner(System.in);
-
-        List<WiseSaying> wiseSayings = new ArrayList<>();
-        int wiseSayingLastId = 0;
+        
         outer:
         while(true) {
             System.out.print("명령) ");
@@ -22,51 +27,63 @@ public class App {
                 case "종료":
                     break outer;
                 case "등록":
-                    int id = ++wiseSayingLastId;
-                    System.out.print("명언 : ");
-                    String contents = sc.nextLine().trim();
-                    System.out.print("작가 : ");
-                    String author = sc.nextLine().trim();
-
-                    WiseSaying wiseSaying = new WiseSaying(id, contents, author);
-                    wiseSayings.add(wiseSaying);
-                    System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+                    write(rq);
                     break;
                 case "삭제":
-                    int paramId = rq.getIntParam("id",0);
-
-                    if(paramId == 0) {
-                        System.out.println("id를 입력해주세요.");
-                        continue;
-                    }
-
-                    WiseSaying wiseSaying__ = null;
-
-                    for(WiseSaying wiseSaying___ : wiseSayings) {
-                        if(wiseSaying___.id == paramId) {
-                            wiseSaying__ = wiseSaying___;
-                        }
-                    }
-
-                    if(wiseSaying__ == null) {
-                        System.out.printf("%d번 명언은 존재하지 않습니다..\n", paramId);
-                        continue;
-                    }
-
-                    wiseSayings.remove(wiseSaying__);
-
-                    System.out.printf("%d번 명언이 삭제되었습니다.\n", paramId);
+                    remove(rq);
                     break;
                 case "목록":
-                    System.out.println("번호 / 작가 / 명언");
-                    System.out.println("------------------");
-                    for(int i = wiseSayings.size()-1; i >= 0; i--) {
-                        WiseSaying wiseSaying_ = wiseSayings.get(i);
-                        System.out.printf("%d / %s / %s\n", wiseSaying_.id, wiseSaying_.contents, wiseSaying_.author);
-                    }
+                    list(rq);
                     break;
             }
         }
         sc.close();
+    }
+
+    private void list(Rq rq) {
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("------------------");
+        for(int i = wiseSayings.size()-1; i >= 0; i--) {
+            WiseSaying wiseSaying_ = wiseSayings.get(i);
+            System.out.printf("%d / %s / %s\n", wiseSaying_.id, wiseSaying_.contents, wiseSaying_.author);
+        }
+    }
+
+    private void remove(Rq rq) {
+        int paramId = rq.getIntParam("id",0);
+
+        if(paramId == 0) {
+            System.out.println("id를 입력해주세요.");
+            return;
+        }
+
+        WiseSaying foundWiseSaying = null;
+
+        for(WiseSaying wiseSaying___ : wiseSayings) {
+            if(wiseSaying___.id == paramId) {
+                foundWiseSaying = wiseSaying___;
+            }
+        }
+
+        if(foundWiseSaying == null) {
+            System.out.printf("%d번 명언은 존재하지 않습니다..\n", paramId);
+            return;
+        }
+
+        wiseSayings.remove(foundWiseSaying);
+
+        System.out.printf("%d번 명언이 삭제되었습니다.\n", paramId);
+    }
+
+    private void write(Rq rq) {
+        int id = ++wiseSayingLastId;
+        System.out.print("명언 : ");
+        String contents = sc.nextLine().trim();
+        System.out.print("작가 : ");
+        String author = sc.nextLine().trim();
+
+        WiseSaying wiseSaying = new WiseSaying(id, contents, author);
+        wiseSayings.add(wiseSaying);
+        System.out.printf("%d번 명언이 등록되었습니다.\n", id);
     }
 }
